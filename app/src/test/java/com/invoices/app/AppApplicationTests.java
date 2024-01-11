580694a6188645e7ce3e13ee23e247e5e57935d0
FlMatamoros <tyndareus1274@gmail.com>
2024-01-11 21:21:52 +0100

    Initial commit

---
+++ app/src/test/java/com/invoices/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.invoices.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
