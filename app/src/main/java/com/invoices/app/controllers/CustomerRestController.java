c22b8851c84116d5beb6785c88abffabb18471ea
FlMatamoros <tyndareus1274@gmail.com>
2024-01-13 16:38:39 +0100

    CRUD Customer done

--- app/src/main/java/com/invoices/app/controllers/CustomerRestController.java
+++ app/src/main/java/com/invoices/app/controllers/CustomerRestController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -60,10 +61,15 @@ public class CustomerRestController {
       return ResponseEntity.ok(customer);
   }
 
-  @PostMapping("/{id}")
+  @PostMapping()
   public ResponseEntity<Customer> saveCustomer(@RequestBody Customer newCustomer) {
     customerService.saveCustomer(newCustomer);
     return ResponseEntity.ok(newCustomer);
   }
-  
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> deleteCustomer(@PathVariable Long id) {
+    customerService.deleteCustomer(id);
+    return ResponseEntity.noContent().build();
+  }
 }
--- app/src/main/java/com/invoices/app/models/services/CustomerServiceImpl.java
+++ app/src/main/java/com/invoices/app/models/services/CustomerServiceImpl.java
@@ -2,6 +2,7 @@ package com.invoices.app.models.services;
 
 import java.util.List;
 
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
@@ -12,14 +13,13 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 import com.invoices.app.models.dao.ICustomerDao;
 import com.invoices.app.models.entities.Customer;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class CustomerServiceImpl implements ICustomerService {
 
   private final ICustomerDao customerDao;
-
-  public CustomerServiceImpl(ICustomerDao customerDao) {
-    this.customerDao = customerDao;
-  }
 
   @Override
   @Transactional(readOnly = true)
@@ -33,7 +33,6 @@ public class CustomerServiceImpl implements ICustomerService {
     if (id == null) {
       throw new IllegalArgumentException("Customer ID can't be null");
     }
-
     return customerDao.findById(id)
         .orElseThrow(() -> new CustomerNotFoundException("Customer not found"));
   }
@@ -45,12 +44,19 @@ public class CustomerServiceImpl implements ICustomerService {
     if (customer == null) {
       throw new IllegalArgumentException("The customer cannot be null");
     }
-    return customerDao.save(customer);
+    try {
+      return customerDao.save(customer);
+    } catch (DataIntegrityViolationException e) {
+      if (e.getMessage().contains("Duplicate entry")) {
+        throw new CustomerEmailAlreadyExistsException("Email already exists: " + customer.getEmail());
+      } else {
+        throw new CustomerSaveException("Error saving customer: Unable to save customer information", e);
+      }
+    }
   }
 
   @Override
   public void deleteCustomer(Long id) {
-
     if (id == null) {
       throw new IllegalArgumentException("The customer id cannot be null");
     }
@@ -65,11 +71,24 @@ public class CustomerServiceImpl implements ICustomerService {
     return customerDao.findAll(pageable);
   }
 
+  // * error control
+
   @ResponseStatus(HttpStatus.NOT_FOUND)
   public static class CustomerNotFoundException extends RuntimeException {
-
     public CustomerNotFoundException(String message) {
       super(message);
+    }
+  }
+  @ResponseStatus(HttpStatus.BAD_REQUEST)
+  public class CustomerEmailAlreadyExistsException extends RuntimeException {
+    public CustomerEmailAlreadyExistsException(String message) {
+      super(message);
+    }
+  }
+  @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+  public class CustomerSaveException extends RuntimeException {
+    public CustomerSaveException(String message, Throwable cause) {
+      super(message, cause);
     }
   }
 
